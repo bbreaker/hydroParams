526d17ded461c2f38a66719e41acd5947d6cc75c
Breaker <1540663030@mil>
2017-12-26 15:49:04 -0600

    added uv function

---
+++ R/recessKuv.R
@@ -0,0 +1,64 @@
+recessKuv <- function(flow, dates) {
+  
+  library(dplyr, quietly = TRUE)
+  
+  if (any(is.na(flow))) {
+    
+    retVal <- NA
+    
+  } else {
+    
+    testDF <- data.frame(dates = dates, flow = flow)
+    
+    testDF$diffQ <- testDF$flow - testDF$baseQ
+    
+    testDF$diffLog <- dplyr::if_else(testDF$diffQ < 0, "fall", "rise")
+    
+    testRle <- data.frame(lengths = rle(testDF$diffLog)$lengths,
+                          vals = rle(testDF$diffLog)$values)
+    
+    testRle$cumVal <- cumsum(testRle$lengths)
+    
+    testRle <- testRle %>% 
+      mutate(qualk = if_else(vals == "fall", 0, 1)) %>% 
+      group_by(qualk) %>% 
+      mutate(index = ifelse(qualk == 0, 1:n(), 0)) %>% 
+      data.frame()
+    
+    indexVal <- na.omit(as.numeric(testRle$index))
+    
+    indexVal <- indexVal[indexVal > 0]
+    
+    kValEvent <- as.numeric()
+    
+    for (i in seq(1, length(indexVal), 1)) {
+      
+      chunk <- testRleDF[(which(testRleDF$index == i) - 1):(which(testRleDF$index == i) + 1), ]
+      
+      chunk$slope <- c(NA, diff(chunk$Flow_Inst) / diff(as.numeric(chunk$dateTime)))
+      
+      chunk <- testDF[(chunk[2, 3]):(chunk[nrow(chunk), 3]), ]
+      
+      chunk$baseQ <- dplyr::if_else(chunk$baseQ == 0, 0.0001, chunk$baseQ)
+      
+      if (nrow(chunk) <= 4) {
+        
+        kValEvent_ <- NA
+        
+      } else {
+        
+        kValEvent_ <- chunk[2, 2] / chunk[1, 2]
+        
+        kValEvent <- append(kValEvent, kValEvent_)
+        
+      }
+      
+    }
+    
+    kVal <- signif(quantile(kValEvent, probs = 0.01, na.rm = TRUE), 3)
+    
+  }
+  
+  return(kVal)
+  
+}
