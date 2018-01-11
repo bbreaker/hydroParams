b690e5bb3a5c5c286c34c5ff4e18777b7185e224
bbreaker <breakerb@gmail.com>
2018-01-11 15:36:30 -0600

    updates

--- R/recessKuv.R
+++ R/recessKuv.R
@@ -18,10 +18,11 @@ recessKuv <- function(flow, dates, nDays) {
     
     testDF <- data.frame(dates = dates, flow = flow)
     
-    nDayVal <- testDF %>% 
-      group_by(dayT = as.Date(testDF$dates)) %>%
-      summarize(lDayT = length(dayT)) %>% 
-      summarize(totDay = round(max(lDayT) * nDays, 0)) %>% 
+    nDayVal <- testDF %>%
+      dplyr::mutate(dayT = as.Date(testDF$dates)) %>% 
+      group_by(dayT) %>% 
+      dplyr::summarize(lDayT = length(dayT)) %>% 
+      dplyr::summarize(totDay = round(max(lDayT) * nDays, 0)) %>% 
       unlist(c())
     
     testDF$numDate <- as.numeric(testDF$dates)
@@ -43,24 +44,77 @@ recessKuv <- function(flow, dates, nDays) {
     
     testDFnon <- na.omit(testDF)
     
-    gamMod <- gamlss(slope ~ pb(numDate, df = 1), data = na.omit(testDF), 
-                     family = LO(mu.link = "identity", sigma.link = "log"))
     
-    testDFnon$muP <- predict(gamMod, what = "mu", data = testDFnon)
+    #gamMod <- gamlss(slope ~ pb(numDate, df = 1), data = na.omit(testDF), 
+                     #family = LO(mu.link = "identity", sigma.link = "log"))
     
-    testDFnon$sigP <- exp(predict(gamMod, what = "sigma", data = testDFnon))
+    testDFnon <- dplyr::mutate(testDFnon, dayVal = format(dates, "%Y-%m-%d"))
     
+    testDates <- unique(testDFnon$dayVal)
+    
+    testVals <- testDFnon[0,]
+    
+    for (i in 1:length(testDates)) {
+      
+      testDFsub <- dplyr::filter(testDFnon, dayVal == testDates[i])
+      
+      gamMod <- tryCatch({
+        
+        gamlss(slope ~ pb(numDate, df = 1), data = testDFsub, 
+               family = LO(mu.link = "identity", sigma.link = "log"))
+        
+      },
+      
+      error = function(cond) {
+        
+        "failure"
+        
+      })
+      
+      if (gamMod == "failure") {
+        
+        testDFsub <- testDFsub %>% 
+          dplyr::mutate(muP = NA)  %>% 
+          dplyr::mutate(sigP = NA) 
+          #dplyr::mutate(slpThrshldHgh = NA) %>%
+          #dplyr::mutate(slpThrshldLow = NA)
+        
+      } else {
+        
+        testDFsub <- testDFsub %>% 
+          dplyr::mutate(muP = predict(gamMod, what = "mu", data = testDFsub))  %>% 
+          dplyr::mutate(sigP = exp(predict(gamMod, what = "sigma", data = testDFsub)))
+          #dplyr::mutate(slpThrshldHgh = qLO(0.55, mu = mean(muP), sigma = mean(sigP))) %>%
+          #dplyr::mutate(slpThrshldLow = qLO(0.45, mu = mean(muP), sigma = mean(sigP)))
+        
+      }
+      
+      testVals <- dplyr::bind_rows(testVals, testDFsub)
+      
+    }
+    
+    testVals$sigP <- if_else(testVals$sigP > quantile(testVals$sigP, probs = 0.99, na.rm = TRUE),
+                             quantile(testVals$sigP, probs = 0.99, na.rm = TRUE), testVals$sigP)
+    
+    testVals$muP <- zoo::na.approx(testVals$muP)
+    
+    testVals$sigP <- zoo::na.approx(testVals$sigP)
+    
+    testVals <- testVals %>% 
+      dplyr::mutate(slpThrshldHgh = qLO(0.75, mu = muP, sigma = sigP)) %>% 
+      dplyr::mutate(slpThrshldLow = qLO(0.25, mu = muP, sigma = sigP))
+      
+    #testDFnon$muP <- predict(gamMod, what = "mu", data = testDFnon)
+    #
+    #testDFnon$sigP <- exp(predict(gamMod, what = "sigma", data = testDFnon))
+    #
     #testDFnon$nuP <- predict(gamMod, what = "nu", data = testDFnon)
-    
-    testDFnon <- testDFnon %>% 
-      mutate(mnth = format(dates, "%m")) %>% 
-      group_by_at(vars(mnth)) %>% 
-      mutate(slpThrshldHgh = qLO(0.55, mu = mean(muP), sigma = mean(sigP))) %>% 
-      mutate(slpThrshldLow = qLO(0.45, mu = mean(muP), sigma = mean(sigP)))
-    
-    slpThrshldHgh <- qLO(0.55, mu=mean(testDFnon$muP), sigma=mean(testDFnon$sigP))
-    
-    slpThrshldLow <- qLO(0.45, mu=mean(testDFnon$muP), sigma=mean(testDFnon$sigP))
+    #
+    #testDFnon <- testDFnon %>% 
+    #  dplyr::mutate(dayVal = format(dates, "%Y-%m-%V")) %>% 
+    #  group_by(dates) %>% 
+    #  dplyr::mutate() %>% 
+    #  dplyr::mutate()
     
     ################ updates to here ##############################################
     
