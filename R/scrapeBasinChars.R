c57d8846b28c473a5bd39c927b15b9e1e9d489b6
Brian Breaker <breakerb@gmail.com>
2024-03-28 10:11:12 -0500

    Update scrapeBasinChars.R

--- R/scrapeBasinChars.R
+++ R/scrapeBasinChars.R
@@ -1,73 +1,31 @@
 scrapeBasinChars <- function(siteIds) { 
   
-  library(rvest) 
+  library(jsonlite) 
   library(dplyr) 
-  library(stringr) 
   
-  results <- data.frame(site_no = as.character(), 
-                        statGrp = as.character(), 
-                        stat = as.character(), 
-                        val = as.character(), 
-                        unit = as.character(), 
-                        stringsAsFactors = FALSE)
+  results <- data.frame()
   
   for (i in 1:length(siteIds)) {
     
     N <- length(siteIds)
     station <- siteIds[i]
-    url <- paste0("https://streamstatsags.cr.usgs.gov/gagepages/html/", station, ".htm")
+    url <- paste0("https://streamstats.usgs.gov/gagestatsservices/characteristics?stationIDOrCode=", station)
     print(paste0(round((i / N) * 100, 2), "%", " complete"))
     
     hold <- tryCatch(
-      url %>%
-        read_html() %>%
-        html_nodes(xpath = '/html/body/table[2]') %>%
-        html_table(fill = TRUE) %>%
-        as.data.frame(),
+      fromJSON(url),
       error = function(e) {"failure"}
     )
     
-    if (hold == "failure") {
+    hold$station_id <- station
+    
+    if (length(hold) == 1) {
       
-      finDF <- data.frame(site_no = station, 
-                          statGrp = as.character(), 
-                          stat = "failed",
-                          val = 0,
-                          unit = "failed", 
-                          stringsAsFactors = FALSE)
+      next
       
-    } else {
+    } else { 
       
-      hold <- hold[-c(1:3, 4), c(1:3)]
-      statHdrs <- if_else(hold$X1 == hold$X2, hold$X1, "notAHdr")
-      statHdrs <- statHdrs[!grepl("notAHdr", statHdrs)]
-      statHdrs <- statHdrs[nchar(statHdrs) > 0]
-
-      for (j in 1:length(statHdrs)) {
-        if (j < length(statHdrs)) {
-          holdRef <- hold[c(grep(statHdrs[j], hold$X1):grep(statHdrs[j + 1], hold$X1)), ]
-          holdRef <- holdRef[-c(1, nrow(holdRef)), ]
-          finDF <- data.frame(site_no = station, 
-                              statGrp = statHdrs[j], 
-                              stat = holdRef$X1, 
-                              val = holdRef$X2, 
-                              unit = holdRef$X3, 
-                              stringsAsFactors = FALSE)
-        } else {
-          holdRef <- hold[c(grep(statHdrs[j], hold$X1):nrow(hold)), ]
-          holdRef <- holdRef[-1, ]
-          holdRef <- holdRef[!(holdRef$X1 == ""), ]
-          finDF <- data.frame(site_no = station, 
-                              statGrp = statHdrs[j], 
-                              stat = holdRef$X1, 
-                              val = holdRef$X2, 
-                              unit = holdRef$X3, 
-                              stringsAsFactors = FALSE)
-        }
-        
-        results <- dplyr::bind_rows(results, finDF); rm(finDF)
-        
-      }
+      results <- dplyr::bind_rows(results, hold); rm(finDF)
       
     }
     
