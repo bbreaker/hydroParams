0014abf3e8d692722e7ca6e2aa60bf099e794a56
Brian Breaker <breakerb@gmail.com>
2024-06-07 09:52:19 -0500

    Update scrapeAHPSmeta.R

--- R/scrapeAHPSmeta.R
+++ R/scrapeAHPSmeta.R
@@ -1,77 +1,40 @@
-scrapeAHPSmeta <- function(localName, altName = NULL) {
+scrapeAHPSmeta <- function(localName, altName = NULL, getSimpDF = FALSE) {
+
+  library(dplyr); library(jsonlite)
   
-  library(rvest) 
-  library(dplyr) 
-  library(stringr) 
+  url <- paste0("https://api.water.noaa.gov/nwps/v1/gauges/", localName) 
   
-  finList <- list(location = data.frame(stringsAsFactors = FALSE), 
-                  datumInfo = data.frame(stringsAsFactors = FALSE))
+  hold <- tryCatch(
+    fromJSON(url),
+    error = function(e) {"failure"}
+  )
   
-  for (i in 1:length(localName)) {
+  if (getSimpDF == TRUE) {
     
-    N <- length(localName)
-    station <- localName[i]
-    altStation <- altName[i]
-    url <- paste0("https://water.weather.gov/ahps2/metadata.php?wfo=lzk&gage=", station)
-    #print(paste0(round((i / N) * 100, 2), "%", " complete"))
+    retDF <- data.frame() 
     
-    hold <- tryCatch(
-      url %>%
-        read_html() %>% 
-        html_nodes('table'),
-      error = function(e) {"failure"}
-    )
-    
-    if (any(hold == "failure")) {
+    for (i in 1:length(hold$flood$categories)) {
       
-      holdInfo <- list(location = data.frame(localName = localName[i], altName = ifelse(is.null(altName), NA, altName[i]), 
-                                             dec_lat_va = NA, dec_long_va = NA, stringsAsFactors = FALSE),  
-                       datumInfo = data.frame(localName = localName[i], altName = ifelse(is.null(altName), NA, altName[i]), 
-                                              desc = NA, datum = NA, flood = NA, 
-                                              explanation = NA, stringsAsFactors = FALSE))
+      catVals <- data.frame(hold$flood$categories[[i]])
       
-      finList[[1]] <- dplyr::bind_rows(finList[[1]], holdInfo[[1]])
-      finList[[2]] <- dplyr::bind_rows(finList[[2]], holdInfo[[2]])
+      catNames <- names(hold$flood$categories)[i]
       
-    } else {
+      retDF_ <- data.frame(dataType = names(hold$flood$categories)[i]) %>% 
+        dplyr::bind_cols(valStage = catVals$stage, valFlow = catVals$flow)
       
-      holdTables <- hold %>%
-        html_nodes(xpath = paste0('/html/body/table[1]')) %>% 
-        html_table(fill = TRUE) %>% 
-        as.data.frame() %>% 
-        dplyr::select(X1, X2, X3, X4)
-      
-      loc <- unlist(str_split_fixed(holdTables[2, 1], ",", n = 3))
-      
-      dec_lat <- unlist(str_split_fixed(loc[1, 1], ": ", n = 2)) [2]
-      
-      dec_lat <- as.numeric(gsub("° N", "", dec_lat))
-      
-      dec_long <- unlist(str_split_fixed(loc[1, 2], ": ", n = 2)) [2]
-      
-      dec_long <- as.numeric(gsub("° W", "", dec_long))
-      
-      desc <- holdTables[4:8, 1]
-      
-      datum <- holdTables[4:8, 2]
-      
-      flood <- holdTables[4:8, 3]
-      
-      explanation <- holdTables[4:8, 4]
-      
-      holdInfo <- list(location = data.frame(localName = localName[i], altName = ifelse(is.null(altName), NA, altName[i]), 
-                                             dec_lat = dec_lat, dec_long = dec_long, stringsAsFactors = FALSE), 
-                       datumInfo = data.frame(localName = localName[i], altName = ifelse(is.null(altName), NA, altName[i]), 
-                                              desc = desc, datum = datum, flood = flood, 
-                                              explanation = explanation, stringsAsFactors = FALSE))
-      
-      finList[[1]] <- dplyr::bind_rows(finList[[1]], holdInfo[[1]])
-      finList[[2]] <- dplyr::bind_rows(finList[[2]], holdInfo[[2]])
-      
+      retDF <- dplyr::bind_rows(retDF, retDF_)
+        
     }
+    
+    retDF_ <- data.frame(dataType = c("conversionElevUnit", "conversionElev"), 
+                         valStage = c(hold$datums$vertical$value$abbrev, hold$datums$vertical$value$value))
+    
+    retDF <- dplyr::bind_rows(retDF, retDF_)
+    
+    return(retDF)
     
   }
   
-  return(finList)
-  
+  return(hold) 
+
 }
