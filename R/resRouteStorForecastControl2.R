27d8a2af5502b46d5b59e75c82ab1e742cb25412
Brian Breaker <breakerb@gmail.com>
2025-12-09 10:50:28 -0600

    Update resRouteStorForecastControl2.R

--- R/resRouteStorForecastControl2.R
+++ R/resRouteStorForecastControl2.R
@@ -1,3 +1,49 @@
+## NEEDS MORE TESTING
+## 'inflow_cfs_series' 
+##  - numeric vector of hourly inflows in cfs
+## 'geometry_curve'
+##  - data frame that contains 3 columns... 'elevation', 'storage', and 'discharge'
+## 'initial_storage_af' 
+##  - single numeric value in acre-feet that represents the storage value at the 
+##    beginning of the simulation
+## 'forecast_hours' 
+##  - single numeric value in hours that represents the look-ahead time used to 
+##    determine if the inflows will create elevations greater than 'elevation_max'
+## 'elevation_max'   
+##  - single numeric value in feet that represents the elevation threshold below from which 
+##    the 'max_ramp_rate_cfs' applies to changes in releases
+## 'elevation_target'
+##  - single numeric value in feet that the simulation tries to stay below within the rules 
+##    that are applied when forecast elevations are below 'elevation_max' and other 
+##    thresholds related to 'max_ramp_rate', 'discharge_update_interval', and etc
+## 'target_elevation' 
+##  - single numeric value in feet that the function tries to maintain by chagning the releases 
+##    when the forecast is not going to above 'elevation_max' (ie top of conservation pool)
+## 'max_discharge_cfs'
+##  - single numeric value in cfs that specifies the max discharge to use unless the forecast shows
+##    elevations exceeding 'elevation_max'
+## 'min_discharge_cfs'
+##  - single numeric value in cfs that specifies a minimum discharge
+## 'max_ramp_rate' - 
+##  - single numeric value in cfs that represents the maximum change in discharge that can 
+##    occur per hour unless the 'ramp_rate_override_elevation' is exceeded
+## 'fixed_discharge_ts' 
+##  - numeric vector of hourly discharge values that can be shorter than the
+##    length of the 'inflow_series_cfs' vector
+##  - if the vector is the same length as 'inflow_time_series', the 'fixed_discharge_ts' 
+##    will be used for releases for the entire simulation
+##  - if the vector is shorter than 'inflow_time_series', the specified discharge values
+##    are used until they end and then the rules take over
+## 'ramp_rate_override_elevation' 
+##  - single numeric value in feet that specifies an elevation above which the 'max_ramp_rate_cfs' 
+##    is disregarded and the geometry curve is used to compute discharges, elevations, and etc 
+## 'discharge_update_interval'
+##  - single numeric value in hours representing time in between discharge changes in discharges
+##  - intended to mimic rate at which gate changes can be made
+## 'discharge_update_interval_override_elevation'
+##  - single numeric value in feet that specifies an elevation at which the 'discharge_update_interval'
+##    goes back to the time step of the inflows 
+
 resRouteStorForecastControl_fixed <- function(inflow_cfs_series,
                                               geometry_curve,
                                               initial_storage_af,
