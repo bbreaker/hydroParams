9cb40b0b53723e6cee20d853d3f88b5c694e9c33
Brian Breaker <breakerb@gmail.com>
2026-03-13 08:47:42 -0500

    Update scrapeCWMS_Rat.R

--- R/scrapeCWMS_Rat.R
+++ R/scrapeCWMS_Rat.R
@@ -1,99 +1,30 @@
 ## startDate and/or endDate should be a character object that resembles a POSIXct object... ie "2024-02-01 14:45:00"
 ## startDate and endDate can be left as NULL.
 
-scrapeCWMS_Rat <- function(rating_id, office, startDate = NULL, endDate = NULL) {
+scrapeCWMS_Rat <- function(rating_id, office) {
   
   rating_id <- stringr::str_replace_all(rating_id, "&", "%26")
   
-  if (!is.null(startDate) & is.null(endDate)) {
-    
-    newStart <- paste0(format(as.POSIXct(startDate), "%Y-%m-%d"), "T", 
-                       format(as.POSIXct(startDate), "%H"), "%3A", 
-                       format(as.POSIXct(startDate), "%M"))
-    
-    url <- paste0("https://cwms-data.usace.army.mil/cwms-data/ratings/", 
-                  rating_id, 
-                  "?office=", office, 
-                  "&begin=", newStart)
-    
-    url <- stringr::str_replace_all(url, " ", "%20")
-    url <- stringr::str_replace_all(url, ";", "%3B")
-    
-    hold <- tryCatch(
-      fromJSON(url),
-      error = function(e) {"failure"}
-    )
-    
-  } else if (!is.null(startDate) & !is.null(endDate)) {
-    
-    newStart <- paste0(format(as.POSIXct(startDate), "%Y-%m-%d"), "T", 
-                       format(as.POSIXct(startDate), "%H"), "%3A", 
-                       format(as.POSIXct(startDate), "%M"))
-    
-    newEnd <- paste0(format(as.POSIXct(endDate), "%Y-%m-%d"), "T", 
-                     format(as.POSIXct(endDate), "%H"), "%3A", 
-                     format(as.POSIXct(endDate), "%M"))
-    
-    url <- paste0("https://cwms-data.usace.army.mil/cwms-data/ratings/", 
-                  rating_id, 
-                  "?office=", office, 
-                  "&begin=", newStart, 
-                  "&end=", newEnd)
-    
-    url <- stringr::str_replace_all(url, " ", "%20")
-    url <- stringr::str_replace_all(url, ";", "%3B")
-    
-    hold <- tryCatch(
-      fromJSON(url),
-      error = function(e) {"failure"}
-    )
-    
-  } else {
-    
-    url <- paste0("https://cwms-data.usace.army.mil/cwms-data/ratings/", 
-                  rating_id, 
-                  "?office=", office)
-    
-    url <- stringr::str_replace_all(url, " ", "%20")
-    url <- stringr::str_replace_all(url, ";", "%3B")
-    
-    hold <- tryCatch(
-      fromJSON(url),
-      error = function(e) {"failure"}
-    )
-    
-  }
+  url <- paste0("https://cwms-data.usace.army.mil/cwms-data/ratings?name=", 
+                rating_id, 
+                "&office=", office)
   
-  newDat <- data.frame(hold[[4]]$`rating-points`)
+  url <- stringr::str_replace_all(url, " ", "%20")
+  url <- stringr::str_replace_all(url, ";", "%3B")
   
-  if (any(purrr::map_lgl(newDat, is.list) == TRUE)) {
-    
-    for (i in 1:nrow(newDat)) {
-      
-      if (i == 1) {
-        
-        retRat <- newDat[[1]][[i]]
-      } else {
-        
-        retRat <- dplyr::bind_rows(retRat, newDat[[1]][[i]])
-      }
-    }
-    
-    retRat <- retRat %>% 
-      dplyr::mutate(isDup = duplicated(ind)) %>% 
-      dplyr::filter(isDup != TRUE) %>% 
-      dplyr::select(-isDup) %>% 
-      dplyr::mutate(ind = as.numeric(ind), 
-                    dep = as.numeric(dep)) %>% 
-      dplyr::arrange(ind)
-  } else {
-    
-    retRat <- newDat %>% 
-      dplyr::mutate(ind = as.numeric(point.ind), 
-                    dep = as.numeric(point.dep)) %>% 
-      dplyr::select(ind, dep) %>% 
-      dplyr::arrange(ind)
-  }
+  library(curl)
+  h <- new_handle(ssl_verifypeer = 0L)
   
-  return(retRat)
+  req <- curl_fetch_memory(url, handle = h)
+  
+  parsed_xml <- xml2::read_xml(req$content)
+  
+  rat <- xml2::xml_text(xml2::xml_find_all(parsed_xml, ".//rating-points"))
+  
+  ratExp <- unlist(stringr::str_split(rat, pattern = "\n"))
+  
+  ratTable <- data.frame(DEP = as.numeric(stringr::str_split_i(ratExp, " ", 1)), 
+                         INDEP = as.numeric(stringr::str_split_i(ratExp, " ", 2)))
+  
+  return(ratTable)
 }
